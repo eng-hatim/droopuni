eef8f4c7292943993cf784684b2e44323e84a24f
Hatim Hussein <hatim201499@gmail.com>
2020-11-22 15:09:50 +0200

    universites page

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $('#test, #play-icon').on('click', function(ev) {
+        $("#video")[0].src += "?autoplay=1";
+        $('#test, #play-icon').remove();   
+    });
+
+    $('.owl-carousel').owlCarousel({
+        rtl:true,
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: false,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })    
+
+    $('#other-click').click(function(e){
+        $('#slide-content').slideToggle('fast');
+
+        for(i = 0; i < 1; i++) {
+            $('#slide-content').animate({marginTop: '-='+'10px'},100).animate({marginTop: '+='+'10px'},100);
+        }        
+
+        $('#slide-content').css('display', 'flex');
+    });    
+    
+    
+
+  });
+
+
+
+
+
